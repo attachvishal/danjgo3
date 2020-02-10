ea95e666609dd5b6546ab44cf581ffcee0563909
attachvishal <attachvishal@yahoo.com>
2020-02-10 11:26:30 +0000

    New updates for Heroku deployment

--- test_project/settings.py
+++ test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5s3ur33-bi$^fjgshtue%7)%*hra$xglfrs%i-qw-tw3npnfu%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'test_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
